88b77045670792e45e3de21fd900a5d7cdd19174
arc3us <arinjayjain380@gmail.com>
2021-10-16 19:38:28 +0530

    Navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    
+      <div className="navbar bg-neutral text-neutral-content shadow-lg">
+        <div className="navbar-start mx-4">
+          <span className="text-lg">Manga</span>
+        </div>
+
+        <div className="navbar-center w-1/2">
+          <div class="flex-1 flex-grow">
+            <div class="form-control text-xl">
+              <input
+                type="text"
+                placeholder="Search"
+                class="input input-ghost"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div class="flex items-stretch">
+            <a class="btn btn-ghost btn-sm rounded-btn">Genres</a>
+            <a class="btn btn-ghost btn-sm rounded-btn">Lists</a>
+            <a class="btn btn-ghost btn-sm rounded-btn">Login</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
