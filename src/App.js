fd07147a077f92a4dfb1bf58538a952bc4ac0b0d
arc3us <arinjayjain380@gmail.com>
2021-10-18 03:01:41 +0530

    dummy data added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Card from "./Card.js";
 import Auth from "./Auth.js";
 
 const name="trial"
+const artlink="https://thiscatdoesnotexist.com/"
+const desc="This is a manga about something something"
+const genre="fantasy"
+const altgenre="mecha"
 
 function App() {
   return (
     <div>
           <Navi />
-          <Card name={name}/>
+          <Card name={name} artlink={artlink} desc={desc} genre={genre} altgenre={altgenre}/>
     </div>
 
   );
