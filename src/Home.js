ae18bac31295db2032c78140d3c68b8387706546
arc3us <arinjayjain380@gmail.com>
2021-11-29 13:35:27 +0530

    Minor fixes

--- src/Home.js
+++ src/Home.js
@@ -9,16 +9,16 @@ const Home = (props) => {
             id: 1, name: "Manga name 1", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
         },
         {
-            id: 2, name: "Manga name 1", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
+            id: 2, name: "Manga name 2", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
         },
         {
-            id: 3, name: "Manga name 1", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
+            id: 3, name: "Manga name 3", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
         },
         {
-            id: 4, name: "Manga name 1", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
+            id: 4, name: "Manga name 4", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
         },
         {
-            id: 5, name: "Manga name 1", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
+            id: 5, name: "Manga name 5", art: "https://thiscatdoesnotexist.com/", des: "this is a manga about xyz stuff", g1: "crime", g2: "mecha"
         },
         
     ]
